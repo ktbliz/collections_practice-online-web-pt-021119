0f4ebf08d83e8882a1c241eb6ae7775670183234
Katie Blizzard <kblizzard11@gmail.com>
2019-02-22 03:07:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,7 +7,13 @@ def sort_array_desc(array)
 end 
 
 def sort_array_char_count(array) 
-
   new_array = array.sort { |x,y|  x.length <=> y.length } 
+end 
 
+def swap_elements(array)
+  second = array[1]
+  third = array[2]
+  array[1] = third
+  array[2] = second
+  array 
 end 
